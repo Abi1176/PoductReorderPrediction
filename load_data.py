d50108ad834c3429230aa4cbdf31749a1179b427
Abi1176 <abinaya.rc@gmail.com>
2025-07-10 15:13:23 +0530

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -7,18 +7,6 @@ def load_all_data():
     Returns:
         dict: A dictionary with DataFrames for each dataset.
     """
-    # datasets = {}
-    # datasets['train'] = pd.read_csv('data/train.csv')
-    # datasets['test'] = pd.read_csv('data/test.csv')
-    # datasets['validation'] = pd.read_csv('data/validation.csv')
-    # orders= pd.read_csv('data/orders.csv')
-    # products = pd.read_csv('data/products.csv')
-    # customers = pd.read_csv('data/customers.csv')
-    # datasets = {
-    #     'orders': orders,
-    #     'products': products,
-    #     'customers': customers
-    # }
     orders = pd.read_csv('data/orders.csv')
     prior = pd.read_csv('data/order_products__prior.csv')
     train = pd.read_csv('data/order_products__train.csv')
